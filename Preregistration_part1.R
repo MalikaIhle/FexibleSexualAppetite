0ce687260edb079fb601d83d8231e318aa8cb935
Malika Ihle <malika_ihle@hotmail.fr>
2017-04-22 19:49:57 -0400

    revosed mod 3 to be CannibalizedRedYN

--- Preregistration_part1.R
+++ Preregistration_part1.R
@@ -9,62 +9,104 @@
 rm(list = ls(all = TRUE))
 
 {# packages
-library(pbapply)
 library(lme4)
-library(dplyr)
 }
 
-# simulation data
+{# simulation data
 
 nF <- 100
 
 FID <- 1:nF
 Trt <- rep(c("RedPref","RedAverse"),nF/2)
-Females_and_Trt <- data.frame(FID,Trt)
-Double_Females_and_Trt <- data.frame(rbind(Females_and_Trt,Females_and_Trt))
-Double_Females_and_Trt <- Double_Females_and_Trt[order(Double_Females_and_Trt$FID),]
-Triple_Females_and_Trt <- data.frame(rbind(Females_and_Trt,Females_and_Trt,Females_and_Trt))
-Triple_Females_and_Trt <- Triple_Females_and_Trt[order(Triple_Females_and_Trt$FID),]
-
 AttackBugYN <- sample(c(0,1),nF, replace=TRUE) # step 1
 AttackNewRedYN <- sample(c(0,1),nF, replace=TRUE) # step 2
 Time_to_attack <- rnorm(nF, mean = 15, sd = 7) # step 2 find value previous experiment!!
 
-Fcondition <-  rnorm(nF*2,mean = 3, sd=1) # find real values !!
-before_after <- rep(c("before","after"),nF)
-
-MID <- 1:(nF*2)
-Mcolor <- rep(c("Red","Black"),nF)
-CannibalizedYN <- unlist(replicate(nF,sample(c(0,1), replace=FALSE),simplify = FALSE)) # step 3
-Msize <- rnorm(nF*2,mean = 3, sd=1) # find real values!!
-Mcondition <- rnorm(nF*2,mean = 3, sd=1) # find real values!!
-
-Step <- rep (c("step1", "step2", "step3"),nF*3)
-
-  
 MY_TABLE_FID <- data.frame(FID, Trt, AttackBugYN,AttackNewRedYN,Time_to_attack)
 
-NY_TABLE_FCondition <- data.frame(Fcondition,before_after,Double_Females_and_Trt)
 
-MY_TABLE_MID <- data.frame( Double_Females_and_Trt,MID, Mcolor, Msize, Mcondition, CannibalizedYN)
+Females_and_Trt <- data.frame(FID,Trt)
+Double_Females_and_Trt <- data.frame(rbind(Females_and_Trt,Females_and_Trt))
+sortedDouble_Females_and_Trt <- Double_Females_and_Trt[order(Double_Females_and_Trt$FID),]
 
-MY_TABLE_Step <- data.frame(step = Step,Triple_Females_and_Trt,
-                            attackRedYN = c(AttackBugYN, AttackNewRedYN, MY_TABLE_MID$CannibalizedYN[MY_TABLE_MID$Mcolor == "Red"]))
+Fsize <- sort(rnorm(nF*2,mean = 4, sd=1))# find real values!!
+Fweight <- rnorm(nF*2,mean = 4, sd=1)# find real values!!
+Fcondition <- resid(lm(Fweight~Fsize))
+before_after <- rep(c("before","z_after"),nF)
 
+MY_TABLE_FCondition <- data.frame(Fcondition,before_after,sortedDouble_Females_and_Trt)
+MY_TABLE_FCondition$interaction <- paste(MY_TABLE_FCondition$before_after,MY_TABLE_FCondition$Trt, sep="")
+
+
+MID <- 1:(nF*2)
+Msize <- sort(rnorm(nF*2,mean = 4, sd=1))# find real values!!
+Mweight <- rnorm(nF*2,mean = 4, sd=1)# find real values!!
+Mcondition <- resid(lm(Mweight~Msize))
+
+MY_TABLE_MID <- data.frame(MID, Msize, Mcondition)
+
+is.even <- function(x) x %% 2 == 0 
+
+MY_TABLE_Mpair <- data.frame(MY_TABLE_MID[is.even(MY_TABLE_MID$MID),],MY_TABLE_MID[!is.even(MY_TABLE_MID$MID),])
+colnames(MY_TABLE_Mpair) <- c("MID1","Msize1","Mcondition1","MID2","Msize2", "Mcondition2")
+MY_TABLE_Mpair$Mcolor1 <- sample(c("Red","Black"),nF, replace = TRUE)
+
+for (i in 1:nrow(MY_TABLE_Mpair)){
+  if(MY_TABLE_Mpair$Mcolor1[i] == "Red"){
+    MY_TABLE_Mpair$Mcolor2[i] <- "Black"
+    MY_TABLE_Mpair$DeltaMsize[i] <- MY_TABLE_Mpair$Msize1[i]-MY_TABLE_Mpair$Msize2[i]
+    MY_TABLE_Mpair$DeltaMcondition[i] <- MY_TABLE_Mpair$Mcondition1[i]-MY_TABLE_Mpair$Mcondition2[i]
+  }
+  if(MY_TABLE_Mpair$Mcolor1[i] == "Black"){
+    MY_TABLE_Mpair$Mcolor2[i] <- "Red"
+    MY_TABLE_Mpair$DeltaMsize[i] <- MY_TABLE_Mpair$Msize2[i]-MY_TABLE_Mpair$Msize1[i]
+    MY_TABLE_Mpair$DeltaMcondition[i] <- MY_TABLE_Mpair$Mcondition2[i]-MY_TABLE_Mpair$Mcondition1[i]}
+}
+
+
+CannibalizedRedYN <- sample(c(0,1), nF,replace=TRUE) # step 3
+
+MY_TABLE_FID <- data.frame(MY_TABLE_FID,MY_TABLE_Mpair,CannibalizedRedYN)
+
+
+Males1 <- MY_TABLE_Mpair[,c("MID1","Mcolor1")]
+colnames(Males1) <- c("MID","Mcolor")
+Males2 <- MY_TABLE_Mpair[,c("MID2","Mcolor2")]
+colnames(Males2) <- c("MID","Mcolor")
+  
+MY_TABLE_MID <- merge(MY_TABLE_MID, rbind(Males1,Males2),all.x=TRUE)
+MY_TABLE_MID$latency_to_court <- rnorm(nF*2, mean = 15, sd = 7)
+
+
+Triple_Females_and_Trt <- data.frame(rbind(Females_and_Trt,Females_and_Trt,Females_and_Trt))
+Step <- sort(rep (c("step1", "step2", "step3"),nF))
+attackRedYN <- c(AttackBugYN, AttackNewRedYN, CannibalizedRedYN)
+
+MY_TABLE_Step <- data.frame(Step,Triple_Females_and_Trt,attackRedYN)
+MY_TABLE_Step <- MY_TABLE_Step[order(MY_TABLE_Step$FID),]
+}
 
 head(MY_TABLE_FID)
-head(NY_TABLE_FCondition)
+head(MY_TABLE_FCondition)
 head(MY_TABLE_MID)
 head(MY_TABLE_Step)
 
-  
+
+## Trt Red Averse is the reference (intercept)
+
+
 # step 1
+
 mod1 <- glm (AttackBugYN ~ Trt, "binomial", data = MY_TABLE_FID)
-summary(mod)
+summary(mod1)
 
+  ## to get one sided test p value
+  coef(summary(mod1))[2, 4]/2
 
-summary(lmer (Fcondition ~ before_after * Trt + (1|FID), data = NY_TABLE_FCondition))
-
+  ## to check slope of change of condition over the diet training isn't different between treatment
+  summary(lmer (Fcondition ~ before_after*Trt + (1|FID), data = MY_TABLE_FCondition))
+  ## if they do differ: check which treatment declined in body condition
+  summary(lmer (Fcondition ~ -1+interaction + (1|FID), data = MY_TABLE_FCondition))
 
 
 
@@ -73,22 +115,36 @@ summary(lmer (Fcondition ~ before_after * Trt + (1|FID), data = NY_TABLE_FCondit
 mod2 <- glm (AttackNewRedYN ~ Trt, family = "binomial",  data = MY_TABLE_FID)
 summary(mod2)
 
+  ## to get one sided test p value
+  coef(summary(mod2))[2, 4]/2
 
-t.test (Time_to_attack[Trt == "RedPref"], Time_to_attack[Trt == "RedAverse"], data = MY_TABLE_FID)
+  ## to check equality of motivation to feed
+  shapiro.test(MY_TABLE_FID$Time_to_attack)
+  t.test (Time_to_attack[Trt == "RedPref"], 
+          Time_to_attack[Trt == "RedAverse"], 
+          data = MY_TABLE_FID)
 
 
 
 
 # step 3
 
-mod3 <- glmer (CannibalizedYN ~ Trt + Mcolor+ Msize + Mcondition + (1|FID), family = "binomial", data = MY_TABLE_MID)
+mod3 <- glm (CannibalizedRedYN ~ Trt+ DeltaMsize + DeltaMcondition, family = "binomial", data = MY_TABLE_FID)
 summary(mod3)
 
+  ## to get one sided test p value
+  coef(summary(mod3))[2, 4]/2
+
+  ## to check equality of male motivation to court
+  shapiro.test(MY_TABLE_MID$latency_to_court)
+  t.test (MY_TABLE_MID$latency_to_court[MY_TABLE_MID$Mcolor == "Red"], 
+          MY_TABLE_MID$latency_to_court[MY_TABLE_MID$Mcolor == "Black"])
 
 
+  
 # exploratory analyses: repeatability of female bias, and step "strenght of effect"
 
-mod4 <- glmer (attackRedYN ~ -1+step*Trt + (1|FID), family = "binomial", data=MY_TABLE_Step)
+mod4 <- glmer (attackRedYN ~ Step+Trt + (1|FID), family = "binomial", data=MY_TABLE_Step)
 summary(mod4)
 
 
